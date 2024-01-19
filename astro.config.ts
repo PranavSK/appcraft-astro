cae2236037bfd714b279e274799dfc4eda0d311d
Pranav S Koundinya <pranavpg101@gmail.com>
2024-01-19 16:14:52 +0530

    build(netlify): add netlify integration
    Add the netlify astro integration to add hybrid rendering adapter to the astro site.
    
    close #3

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
+import netlify from '@astrojs/netlify'
 import solidJs from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [tailwind(), solidJs()],
   output: 'hybrid',
   vite: {
